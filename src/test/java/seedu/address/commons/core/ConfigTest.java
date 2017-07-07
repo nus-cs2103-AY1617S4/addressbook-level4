ace2b2d9c68f2b9780b0f68e50e5d196b961585c
DESKTOP-44EVQ3F\Aung Swumm <aungswumm@outlook.com>
2017-07-07 16:47:43 +0800

    Change App Name

--- src/test/java/seedu/address/commons/core/ConfigTest.java
+++ src/test/java/seedu/address/commons/core/ConfigTest.java
@@ -14,7 +14,7 @@
 //
 //    @Test
 //    public void toString_defaultObject_stringReturned() {
-//        String defaultConfigAsString = "App title : Address App\n"
+//        String defaultConfigAsString = "App title : WhatsNext App\n"
 //                + "Current log level : INFO\n"
 //                + "Preference file Location : preferences.json";
 //
