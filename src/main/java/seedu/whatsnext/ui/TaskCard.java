57c3ed1d8d32baf41a11e1bfb388bfc72b362966
DESKTOP-44EVQ3F\Aung Swumm <aungswumm@outlook.com>
2017-07-07 16:42:01 +0800

    Rough Gui

--- src/main/java/seedu/whatsnext/ui/TaskCard.java
+++ src/main/java/seedu/whatsnext/ui/TaskCard.java
@@ -9,7 +9,7 @@ import seedu.whatsnext.model.task.BaseTask;
 
 public class TaskCard extends UiPart<Region> {
 
-    private static final String FXML = "PersonListCard.fxml";
+    private static final String FXML = "TaskListCard.fxml";
 
     /**
      * Note: Certain keywords such as "location" and "resources" are reserved keywords in JavaFX.
@@ -26,12 +26,6 @@ public class TaskCard extends UiPart<Region> {
     @FXML
     private Label id;
     @FXML
-    private Label phone;
-    @FXML
-    private Label address;
-    @FXML
-    private Label email;
-    @FXML
     private FlowPane tags;
 
     public TaskCard(BaseTask task, int displayedIndex) {
@@ -41,7 +35,7 @@ public class TaskCard extends UiPart<Region> {
         initTags(task);
     }
 
-    private void initTags(BaseTask person) {
-        person.getTags().forEach(tag -> tags.getChildren().add(new Label(tag.tagName)));
+    private void initTags(BaseTask task) {
+        task.getTags().forEach(tag -> tags.getChildren().add(new Label(tag.tagName)));
     }
 }
--- src/main/java/seedu/whatsnext/ui/TaskListPanel.java
+++ src/main/java/seedu/whatsnext/ui/TaskListPanel.java
@@ -9,37 +9,39 @@ import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.Region;
 import seedu.whatsnext.commons.core.LogsCenter;
+import seedu.whatsnext.commons.events.ui.TaskPanelSelectionChangedEvent;
 import seedu.whatsnext.model.task.BaseTask;
 
 /**
  * Panel containing the list of persons.
  */
 public class TaskListPanel extends UiPart<Region> {
-    private static final String FXML = "PersonListPanel.fxml";
+    private static final String FXML = "TaskListPanel.fxml";
     private final Logger logger = LogsCenter.getLogger(TaskListPanel.class);
 
     @FXML
     private ListView<BaseTask> taskListView;
 
-    public TaskListPanel(ObservableList<BaseTask> personList) {
+    public TaskListPanel(ObservableList<BaseTask> taskList) {
         super(FXML);
-        setConnections(personList);
+        setConnections(taskList);
     }
 
-    private void setConnections(ObservableList<BaseTask> personList) {
-//        taskListView.setItems(personList);
-//        taskListView.setCellFactory(listView -> new TaskListViewCell());
+    private void setConnections(ObservableList<BaseTask> taskList) {
+    	
+        taskListView.setItems(taskList);
+        taskListView.setCellFactory(listView -> new TaskListViewCell());
         setEventHandlerForSelectionChangeEvent();
     }
 
     private void setEventHandlerForSelectionChangeEvent() {
-//        taskListView.getSelectionModel().selectedItemProperty()
-//                .addListener((observable, oldValue, newValue) -> {
-//                    if (newValue != null) {
-//                        logger.fine("Selection in person list panel changed to : '" + newValue + "'");
-//                        raise(new TaskPanelSelectionChangedEvent(newValue));
-//                    }
-//                });
+        taskListView.getSelectionModel().selectedItemProperty()
+                .addListener((observablse, oldValue, newValue) -> {
+                    if (newValue != null) {
+                        logger.fine("Selection in person list panel changed to : '" + newValue + "'");
+                        raise(new TaskPanelSelectionChangedEvent(newValue));
+                    }
+                });
     }
 
     public void scrollTo(int index) {
@@ -52,14 +54,14 @@ public class TaskListPanel extends UiPart<Region> {
     class TaskListViewCell extends ListCell<BaseTask> {
 
         @Override
-        protected void updateItem(BaseTask person, boolean empty) {
-            super.updateItem(person, empty);
+        protected void updateItem(BaseTask task, boolean empty) {
+            super.updateItem(task, empty);
 
-            if (empty || person == null) {
+            if (empty || task == null) {
                 setGraphic(null);
                 setText(null);
             } else {
-                setGraphic(new TaskCard(person, getIndex() + 1).getRoot());
+                setGraphic(new TaskCard(task, getIndex() + 1).getRoot());
             }
         }
     }
