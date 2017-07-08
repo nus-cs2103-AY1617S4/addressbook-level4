cd16f9adf3057b20fc4e5aa874c0233a4643529c
Chi Shien Tay <Chi Shien Tay>
2017-07-08 22:15:09 +0800

    Completed List Command

--- src/main/java/seedu/whatsnext/model/Model.java
+++ src/main/java/seedu/whatsnext/model/Model.java
@@ -15,17 +15,17 @@ public interface Model {
     /** Clears existing backing model and replaces with the provided new data. */
     void resetData(ReadOnlyTaskManager newData);
 
-    /** Returns the AddressBook */
+    /** Returns the TaskManager */
     ReadOnlyTaskManager getTaskManager();
 
-    /** Deletes the given person. */
+    /** Deletes the given task. */
     void deleteTask(BasicTaskFeatures target) throws TaskNotFoundException;
 
-    /** Adds the given person */
+    /** Adds the given task */
     void addTask(BasicTask basicTask) throws DuplicateTaskException;
 
     /**
-     * Replaces the given person {@code target} with {@code editedPerson}.
+     * Replaces the given task {@code target} with {@code editedTask}.
      *
      * @throws DuplicateTaskException if updating the task's details causes the task to be equivalent to
      *      another existing task in the list.
@@ -37,10 +37,16 @@ public interface Model {
     /** Returns the filtered task list as an {@code UnmodifiableObservableList<BasicTaskFeatures>} */
     UnmodifiableObservableList<BasicTaskFeatures> getFilteredTaskList();
 
-    /** Updates the filter of the filtered task list to show all persons */
+    /** Updates the filter of the filtered task list to show all tasks */
     void updateFilteredListToShowAll();
 
     /** Updates the filter of the filtered task list to filter by the given keywords*/
     void updateFilteredTaskList(Set<String> keywords);
+
+    // @@author A0154986L
+    /**
+     * Updates the filter of the filtered task list to filter by task completion
+     */
+    void updateFilteredTaskListToShowByCompletion(boolean isComplete);
 
 }
