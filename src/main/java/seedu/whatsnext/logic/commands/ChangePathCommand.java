2c44902b274ce225e925695163181fd5a49e50e1
Lim Dao Han <e0014798@u.nus.edu>
2017-07-11 19:52:44 +0800

    Refactored findPath to filepath. Checkstyle error corrections.

--- src/main/java/seedu/whatsnext/logic/commands/ChangePathCommand.java
+++ src/main/java/seedu/whatsnext/logic/commands/ChangePathCommand.java
@@ -2,7 +2,6 @@ package seedu.whatsnext.logic.commands;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
 import java.util.logging.Logger;
 
 import seedu.whatsnext.commons.core.Config;
@@ -19,17 +18,21 @@ import seedu.whatsnext.storage.XmlTaskManagerStorage;
  */
 //@@author A0149894H
 public class ChangePathCommand extends Command {
-    private static final Logger logger = LogsCenter.getLogger(ChangePathCommand.class);
-    public static final String COMMAND_WORD = "changePath";
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Changes where task manager is stored. Takes in new file location";
+    public static final String COMMAND_WORD = "changepath";
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Changes where task manager is stored. Takes in new file location";
     public static final String MESSAGE_CREATED_NEW_CONFIG_FILE = "TEST";
     public static final String MESSAGE_REPEAT_TASK_MANAGER_FILE_PATH = "This file is already saved at this location";
     public static final String MESSAGE_SUCCESS = "Save location changed to: %1$s";
 
-    private final File toSave;
+    private static final Logger logger = LogsCenter.getLogger(ChangePathCommand.class);
     private static String toDelete = Config.getTaskManagerFilePath();
 
-    public ChangePathCommand(File filePath){
+
+    private final File toSave;
+
+
+    public ChangePathCommand(File filePath) {
         this.toSave = filePath;
     }
 
@@ -38,17 +41,16 @@ public class ChangePathCommand extends Command {
         try {
             Config config = new Config();
             config.setTaskManagerFilePath(toSave.toString());
-            ConfigUtil.saveConfig(config, Config.DEFAULT_CONFIG_FILE);           
-            
+            ConfigUtil.saveConfig(config, Config.DEFAULT_CONFIG_FILE);
             File f = new File("test.txt");
             String string = f.getAbsolutePath();
             int texttxtSize = 8;
-            int size = string.length()- texttxtSize;
-            string = string.substring(0,size);
+            int size = string.length() - texttxtSize;
+            string = string.substring(0, size);
             string = string.replace("\\", "/");
             String deleteLocation = String.format(string).concat(toDelete);
             File filePath = new File(deleteLocation);
-            filePath.delete(); 
+            filePath.delete();
 
 
             XmlTaskManagerStorage.changeTaskManagerFilePath(toSave.toString());
@@ -60,8 +62,8 @@ public class ChangePathCommand extends Command {
         } catch (IOException ioe) {
             logger.warning("Failed to save config file : " + StringUtil.getDetails(ioe));
             throw new CommandException(MESSAGE_CREATED_NEW_CONFIG_FILE);
-        }        
-        
+        }
+
     }
 
 
