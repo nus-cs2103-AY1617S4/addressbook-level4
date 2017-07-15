54e58e03be533f109e4b1c51cfe6ee6dd91fb201
luishengjie <luishengjie8@gmail.com>
2017-07-15 16:04:39 +0800

    Resolve Failure in AddCommandTest when iverlap implemented

--- src/test/java/seedu/whatsnext/logic/commands/AddCommandTest.java
+++ src/test/java/seedu/whatsnext/logic/commands/AddCommandTest.java
@@ -11,6 +11,8 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import seedu.whatsnext.commons.core.UnmodifiableObservableList;
 import seedu.whatsnext.commons.exceptions.IllegalValueException;
 import seedu.whatsnext.logic.CommandHistory;
@@ -154,6 +156,7 @@ public class AddCommandTest {
         }
     }
 
+    //@@author A0156106M
     /**
      * A Model stub that always throw a DuplicatePersonException when trying to add a person.
      */
@@ -162,6 +165,15 @@ public class AddCommandTest {
         public void addTask(BasicTask task) throws DuplicateTaskException {
             throw new DuplicateTaskException();
         }
+
+        @Override
+        public UnmodifiableObservableList<BasicTaskFeatures> getFilteredTaskList() {
+            final ArrayList<BasicTask> tasksAdded = new ArrayList<>();
+            ObservableList<BasicTask> observableList = FXCollections.observableArrayList(tasksAdded);
+            return new UnmodifiableObservableList<BasicTaskFeatures>(observableList);
+
+        }
+
     }
 
     //@@author A0156106M
@@ -175,6 +187,15 @@ public class AddCommandTest {
         public void addTask(BasicTask task) throws DuplicateTaskException {
             tasksAdded.add(new BasicTask(task));
         }
+
+        @Override
+        public UnmodifiableObservableList<BasicTaskFeatures> getFilteredTaskList() {
+            ObservableList<BasicTask> observableList = FXCollections.observableArrayList(tasksAdded);
+            return new UnmodifiableObservableList<BasicTaskFeatures>(observableList);
+
+        }
+
+
     }
 
 }
