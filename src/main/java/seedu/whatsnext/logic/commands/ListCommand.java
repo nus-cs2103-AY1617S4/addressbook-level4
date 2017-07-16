d27507f002b94dab38b18a13ea520b335f257198
LISHICHENG\User <lishichengx_ray@hotmail.com>
2017-07-16 21:09:47 +0800

    Fix ListCommand and LIstCommandPasser so wrong commands are thrown in Parser instead.

--- src/main/java/seedu/whatsnext/logic/commands/ListCommand.java
+++ src/main/java/seedu/whatsnext/logic/commands/ListCommand.java
@@ -2,10 +2,8 @@ package seedu.whatsnext.logic.commands;
 
 import java.util.Set;
 
-import seedu.whatsnext.commons.core.Messages;
-import seedu.whatsnext.logic.commands.exceptions.CommandException;
-
 //@@author A0154986L
+//@@author A0142675B
 /**
  * Lists all uncompleted/ completed/ all tasks in the task manager to the user.
  * It can also list tasks by type only.
@@ -17,9 +15,10 @@ public class ListCommand extends Command {
     public static final String LIST_COMPLETED = "completed";
     public static final String LIST_ALL = "all";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Listed all incomplete tasks. "
-            + "Parameters: "
-            + "complete/all: List all complete/all tasks ";
+    public static final String MESSAGE_USAGE = COMMAND_WORD + "list the tasks in the task manager.\n"
+            + "To list incomplete tasks : list OR list incomplete\n"
+            + "To list completed tasks : list completed\n"
+            + "To list all tasks : list all";
 
     public static final String MESSAGE_SUCCESS_INCOMPLETE = "Listed all incomplete tasks";
     public static final String MESSAGE_SUCCESS_COMPLETED = "Listed all complete tasks";
@@ -32,7 +31,7 @@ public class ListCommand extends Command {
     }
 
     @Override
-    public CommandResult execute() throws CommandException {
+    public CommandResult execute() {
         if (keywords.isEmpty() || keywords.contains(LIST_INCOMPLETE)) {
             boolean isComplete = false;
             model.updateFilteredTaskListToShowByCompletion(isComplete);
@@ -41,11 +40,9 @@ public class ListCommand extends Command {
             boolean isComplete = true;
             model.updateFilteredTaskListToShowByCompletion(isComplete);
             return new CommandResult(MESSAGE_SUCCESS_COMPLETED);
-        } else if (keywords.contains(LIST_ALL)) {
+        } else /*if (keywords.contains(LIST_ALL))*/ {
             model.updateFilteredListToShowAll();
             return new CommandResult(MESSAGE_SUCCESS_ALL);
-        } else {
-            throw new CommandException(Messages.MESSAGE_INVALID_LIST_COMMAND);
         }
     }
 }
