06d009d9cecd0d99771ebb21b10cf84b9bc672a4
Herman Wong <herman.wong333@gmail.com>
2017-07-16 18:03:06 +0800

    Add auto complete

--- src/main/java/seedu/ticktask/ui/CommandBox.java
+++ src/main/java/seedu/ticktask/ui/CommandBox.java
@@ -6,23 +6,24 @@ import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Region;
+import org.controlsfx.control.textfield.AutoCompletionBinding;
+import org.controlsfx.control.textfield.TextFields;
 import seedu.ticktask.commons.core.LogsCenter;
 import seedu.ticktask.commons.events.ui.NewResultAvailableEvent;
 import seedu.ticktask.commons.exceptions.IllegalValueException;
 import seedu.ticktask.logic.Logic;
 import seedu.ticktask.logic.commands.CommandResult;
-import seedu.ticktask.logic.commands.CompleteCommand;
-import seedu.ticktask.logic.commands.ConfirmCommand;
 import seedu.ticktask.logic.commands.exceptions.CommandException;
 import seedu.ticktask.logic.commands.exceptions.WarningException;
-import seedu.ticktask.logic.parser.ConfirmCommandParser;
 import seedu.ticktask.logic.parser.exceptions.ParseException;
 
+import java.util.HashSet;
+import java.util.Set;
 import java.util.Stack;
 import java.util.logging.Logger;
 
 public class CommandBox extends UiPart<Region> {
-
+    
     public static final String ERROR_STYLE_CLASS = "error";
     private static final String FXML = "CommandBox.fxml";
     private Stack<String>prevCommandsHistory = new Stack<String>();
@@ -31,13 +32,18 @@ public class CommandBox extends UiPart<Region> {
     private String lastPrev = "";
     private final Logger logger = LogsCenter.getLogger(CommandBox.class);
     private final Logic logic;
-
+    private AutoCompletionBinding<String> autoCompletionBinding;
+    
+    Set listOfCommands = new HashSet<>();
+    String[] commands = { "add", "delete", "edit", "clear", "help", "undo", "redo", "find", "exit", "delete" };
+    
     @FXML
     private TextField commandTextField;
-
+    
     public CommandBox(Logic logic) {
         super(FXML);
         this.logic = logic;
+        autoComplete();
     }
     
     //@@author A0139964M
@@ -75,64 +81,73 @@ public class CommandBox extends UiPart<Region> {
         }
     }
     
+    
+    
     private void updatePrevCommand(String commandText){
         prevCommandsHistory.push(commandText);
+    }
+    
+    public void autoComplete(){
+        buildCommandsIntoHashSet();
+        autoCompletionBinding = TextFields.bindAutoCompletion(commandTextField, listOfCommands);
+        autoCompletionBinding.setPrefWidth(300);
+        autoCompletionBinding.setVisibleRowCount(1);
+        autoCompletionBinding.setHideOnEscape(true);
+    }
+    
+    public void buildCommandsIntoHashSet(){
+        for(int i = 0; i < commands.length; i++){
+            listOfCommands.add(commands[i]);
+        }
     }
     //@@author
     
     @FXML
-    private void handleCommandInputChanged() throws IllegalValueException {
-        String commandText = new String();
+    private void handleCommandInputChanged() throws IllegalValueException, WarningException {
         try {
-            commandText = commandTextField.getText();
+            String commandText = commandTextField.getText();
             currentShownCommand = commandText;
             CommandResult commandResult = logic.execute(commandText);
-
+            
             while(!nextCommandsHistory.isEmpty()){
                 prevCommandsHistory.push(nextCommandsHistory.pop());
             }
             
             updatePrevCommand(commandText);
-    
+            
             // process result of the command
             setStyleToIndicateCommandSuccess();
             commandTextField.setText("");
             logger.info("Result: " + commandResult.feedbackToUser);
             raise(new NewResultAvailableEvent(commandResult.feedbackToUser));
-
-        } catch (WarningException e) {
-            setStyleToIndicateCommandFailure();
-            raise(new NewResultAvailableEvent(e.getMessage()));
-            //String responseText = commandTextField.getText();
-            //boolean response = new ConfirmCommandParser().parse(responseText);
             
         } catch (CommandException | ParseException e) {
             // handle command failure
             setStyleToIndicateCommandFailure();
             logger.info("Invalid command: " + commandTextField.getText());
             raise(new NewResultAvailableEvent(e.getMessage()));
-        } 
+        }
     }
-
-
+    
+    
     /**
      * Sets the command box style to indicate a successful command.
      */
     private void setStyleToIndicateCommandSuccess() {
         commandTextField.getStyleClass().remove(ERROR_STYLE_CLASS);
     }
-
+    
     /**
      * Sets the command box style to indicate a failed command.
      */
     private void setStyleToIndicateCommandFailure() {
         ObservableList<String> styleClass = commandTextField.getStyleClass();
-
+        
         if (styleClass.contains(ERROR_STYLE_CLASS)) {
             return;
         }
-
+        
         styleClass.add(ERROR_STYLE_CLASS);
     }
-
+    
 }
