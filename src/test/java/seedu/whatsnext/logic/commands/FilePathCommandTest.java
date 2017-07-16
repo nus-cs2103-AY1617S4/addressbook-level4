b1f9fcb0ca16d0c3bfd361f7c225a70ff4d85bdb
Lim Dao Han <e0014798@u.nus.edu>
2017-07-16 23:00:11 +0800

    FilePathCommandTest

--- src/test/java/seedu/whatsnext/logic/commands/FilePathCommandTest.java
+++ src/test/java/seedu/whatsnext/logic/commands/FilePathCommandTest.java
@@ -1,6 +1,5 @@
 package seedu.whatsnext.logic.commands;
 
-<<<<<<< HEAD
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
@@ -13,8 +12,7 @@ import seedu.whatsnext.model.task.exceptions.DuplicateTaskException;
  *contains integration tests and unit tests for FilePathCommand
  */
 //@@author A0149894H
-=======
->>>>>>> TeamMain/master
+
 public class FilePathCommandTest {
     public static final String TEST_ORIGINAL_LOCATION = "data";
     public static final String TEST_SECOND_LOCATION = "data/test";
