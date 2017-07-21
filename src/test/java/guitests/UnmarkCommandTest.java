272a24ee28f7794fc89969972ab8da243b157659
chishien <taychishien@gmail.com>
2017-07-21 19:41:06 +0800

    Unmarking already unmarked task test

--- src/test/java/guitests/UnmarkCommandTest.java
+++ src/test/java/guitests/UnmarkCommandTest.java
@@ -3,6 +3,7 @@ package guitests;
 import org.junit.Test;
 
 import seedu.whatsnext.commons.core.Messages;
+import seedu.whatsnext.logic.commands.UnmarkCommand;
 
 public class UnmarkCommandTest extends TaskManagerGuiTest {
 
@@ -29,6 +30,23 @@ public class UnmarkCommandTest extends TaskManagerGuiTest {
         assertListSize(beforeSize - 1);
         commandBox.runCommand("list incomplete");
         assertListSize(incompleteBeforeSize + 1);
+    }
+
+    //@@author A0154986L
+    @Test
+    public void markValidIndexAlreadyUnmarked() {
+        commandBox.pressEnter();
+        int incompleteBeforeSize = eventListPanel.getNumberOfTask() + deadlineListPanel.getNumberOfTask()
+                        + floatingListPanel.getNumberOfTask();
+        commandBox.runCommand("list completed");
+        int beforeSize = eventListPanel.getNumberOfTask() + deadlineListPanel.getNumberOfTask()
+                        + floatingListPanel.getNumberOfTask();
+        commandBox.runCommand("unmark 1");
+        assertListSize(beforeSize - 1);
+        commandBox.runCommand("list incomplete");
+        assertListSize(incompleteBeforeSize + 1);
+        commandBox.runCommand("unmark 1");
+        assertResultMessage(UnmarkCommand.MESSAGE_TASK_UNMARKED);
     }
 
 }
