66d8287ff85c90f5978e7cae2c0a3f70b9e1a7e8
QIGEMINGZIZHENNANA <A0131884@u.nus.edu>
2017-07-23 17:16:39 +0800

    no message

--- src/main/java/seedu/ticktask/logic/commands/DeleteCommand.java
+++ src/main/java/seedu/ticktask/logic/commands/DeleteCommand.java
@@ -23,7 +23,7 @@ public abstract class DeleteCommand extends Command {
             + PREFIX_COMPLETE + " 1";
     public static final String MESSAGE_SUCCESS = "Task deleted:" + "\n"
             + Messages.MESSAGE_TASK_DESCRIPTION + "%1$s";
-    public static final String MESSAGE_WARNING = "The deleted task name is a substring of the stored task name.\n" +
+    public static final String MESSAGE_WARNING = "The deleted task name is a subset of the stored task name.\n" +
             "Please use 'undo' command to restore if the deletion in not desired.\n" +
             "Task deleted:" + "\n"
             + Messages.MESSAGE_TASK_DESCRIPTION + "%1$s";
