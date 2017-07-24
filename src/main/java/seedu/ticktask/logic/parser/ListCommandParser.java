9f8140ac5da3e34663f5cace893a92a6ee29a0be
QIGEMINGZIZHENNANA <A0131884@u.nus.edu>
2017-07-24 16:33:44 +0800

    Update ListCommandParser.java

--- src/main/java/seedu/ticktask/logic/parser/ListCommandParser.java
+++ src/main/java/seedu/ticktask/logic/parser/ListCommandParser.java
@@ -15,7 +15,7 @@ public class ListCommandParser {
      * Parses the given {@code String} of arguments in the context of the ListCommand
      * and returns an ListCommand object for execution.
      */
-	public Command parse(String args) throws ParseException{
+	public Command parse(String args) throws ParseException {
         String typeOfList = args.trim().toLowerCase();
 
         if (!ListCommand.isValidCommand(typeOfList)) {
